d6e37facd58b6f4bda11cf3122c6855abf0ea32b
Elgrogz <gregorgilchrist@gmail.com>
2017-01-27 19:43:11 +0000

    can display date, time and snowfall

--- public/index.js
+++ public/index.js
@@ -1,4 +1,6 @@
-var url = "http://api.openweathermap.org/data/2.5/forecast?q=Edinburgh,uk&mode=json&appid=" + apikey;
+// var url = "http://api.openweathermap.org/data/2.5/forecast?q=Edinburgh,uk&mode=json&appid=" + apikey;
+
+var url = "http://api.weatherunlocked.com/api/resortforecast/1401?app_id=20805c07&app_key=afab5ce56629579eb9db2f259459f349"
 var data = null;
 
 var makeRequest = function(url, callback) {
@@ -13,9 +15,25 @@ var requestComplete = function() {
   var jsonString = this.responseText;
   data = JSON.parse(jsonString);
   console.log(data);
+  generateWeatherData();
+}
+
+var generateWeatherData = function() {
+  var forecasts = document.querySelector('#forecasts');
+  for (forecast of data.forecast) {
+    console.log(forecast.snow_mm);
+    var dateToAdd = document.createElement('ul');
+    dateToAdd.innerText = forecast.date;
+    var timeToAdd = document.createElement('li');
+    timeToAdd.innerText = forecast.time;
+    var snowfall = document.createElement('li');
+    snowfall.innerText = forecast.snow_mm + "mm of snow";
+    dateToAdd.appendChild(timeToAdd);
+    dateToAdd.appendChild(snowfall);
+    forecasts.appendChild(dateToAdd);
+  }
 }
 
 window.onload = function() {
-  console.log("Hello");
   makeRequest(url, requestComplete);
 };
---
+++ public/skiresorts.md
@@ -0,0 +1,6 @@
+1401  Cairngorm Scotland  Snow conditions and forecast
+1398  Glencoe Scotland  Snow conditions and forecast
+1399  Glenshee  Scotland  Snow conditions and forecast
+54886651  Midlothian Ski Centre Scotland  Forecast Only
+1397  Nevis Range Scotland  Snow conditions and forecast
+1400  The Lecht Scotland  Snow conditions and forecast
