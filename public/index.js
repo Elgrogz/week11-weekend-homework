296ec51fdf8339305268a279af5154d864526662
Elgrogz <gregorgilchrist@gmail.com>
2017-01-29 18:29:46 +0000

    have data for current weather in Aviemore

--- public/index.js
+++ public/index.js
@@ -1,8 +1,9 @@
 var urlCairngormSnowDetails = "http://api.weatherunlocked.com/api/resortforecast/1401?app_id=20805c07&app_key=" + apikeyWeatherUnlocked; 
 
-var urlGlenshee = "http://api.openweathermap.org/data/2.5/forecast?lat=56.887023&lon=-3.415439&appid=" + apikeyOpenWeatherMap;
+var urlAviemoreWeatherDetails = "http://api.openweathermap.org/data/2.5/forecast?q=Aviemore,uk&mode=json&appid=" + apikeyOpenWeatherMap;
 
-var data = null;
+var cairngormSnowData = null;
+var aviemoreWeatherData = null;
 
 var makeRequest = function(url, callback) {
   var request = new XMLHttpRequest();
@@ -11,25 +12,43 @@ var makeRequest = function(url, callback) {
   request.send();
 }
 
-var requestComplete = function() {
+var snowRequestComplete = function() {
   if (this.status != 200) return;
   var jsonString = this.responseText;
-  data = JSON.parse(jsonString);
-  console.log(data);
-  generateWeatherData();
-}
-
-var generateWeatherData = function() {
-  var container = document.querySelector('#forecasts');
+  cairngormSnowData = JSON.parse(jsonString);
+  console.log(cairngormSnowData);
+  
+  var container = document.querySelector('#cairngorm-snow-forecast');
   var forecastData = [];
-  for (var forecast of data.forecast) {
+  for (var forecast of cairngormSnowData.forecast) {
     forecastData.push({name:forecast.date + " " + forecast.time, data: [forecast.snow_mm]})
   }
   new ColumnChart(container, forecastData);
 }
 
+var aviemoreRequestComplete = function() {
+  if (this.status != 200) return;
+  var jsonString = this.responseText;
+  aviemoreWeatherData = JSON.parse(jsonString);
+  console.log(aviemoreWeatherData);
+
+  var container = document.querySelector('#aviemore-forecast');
+
+  var weatherData = document.createElement('ul');
+  var currentTemp = document.createElement('li');
+  console.log(aviemoreWeatherData.list[0].main.temp);
+
+  currentTemp.innerText = (aviemoreWeatherData.list[0].main.temp - 273.15).toFixed(2) + "°C";
+  weatherData.appendChild(currentTemp);
+  
+
+  container.appendChild(weatherData);
+  
+}
+
 var initialize = function() {
-  makeRequest(urlCairngormSnowDetails, requestComplete);
+  makeRequest(urlCairngormSnowDetails, snowRequestComplete);
+  makeRequest(urlAviemoreWeatherDetails, aviemoreRequestComplete);
 
   var mapDiv = document.querySelector("#main-map");
   var cairngorm = {lat: 57.133765, lng: -3.670272};
